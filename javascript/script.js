dd9221b672669fe125fc1d801bfa49fc47900b3e
kakerucmd <151794049+kakerucmd@users.noreply.github.com>
2024-06-10 03:38:49 +0900

    小さい画面でハンバーガーメニューに変更

--- javascript/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('menu-toggle').addEventListener('change', function() {
-    document.querySelector('.discord-container').style.marginLeft = this.checked ? '200px' : '0';
-});
